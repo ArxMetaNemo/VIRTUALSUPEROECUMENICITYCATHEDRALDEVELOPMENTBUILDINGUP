151618c0beed054a86c42e0d2f48693d26f50476
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2021-06-03 22:15:03 +0800

    Add files via upload

--- LASTFRAME.mm
+++ LASTFRAME.mm
@@ -3,6 +3,14 @@
     <node TEXT="immortalpjzonepyramidsphinxbot" FOLDED="false" POSITION="right" ID="60b746756c60ef697cc5e9ce" X_COGGLE_POSX="227" X_COGGLE_POSY="-91">
         <edge COLOR="#988ee3"/>
         <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="ВСЯ ДИАГРАММА ---- ---+ ЭТО ПРОГРАММА-МАКСИМУМ" FOLDED="false" POSITION="left" ID="0130db64cdaca7ff12d89e02">
+            <edge COLOR="#948ce3"/>
+            <font NAME="Helvetica" SIZE="50"/>
+        </node>
+        <node TEXT="эта диаграмма ультимативно ультраметаэволюционирует в реальном времени" FOLDED="false" POSITION="right" ID="864b6ca03c2200f1cb26861a">
+            <edge COLOR="#9188e1"/>
+            <font NAME="Helvetica" SIZE="50"/>
+        </node>
         <node TEXT="https://github.com/LIFEDAO" FOLDED="false" POSITION="left" ID="5760d56d2ebed08f58f22303">
             <edge COLOR="#9ed56b"/>
             <font NAME="Helvetica" SIZE="45"/>
@@ -34,28 +42,49 @@
         <node TEXT="OpenMendelevium" FOLDED="false" POSITION="left" ID="fe2f09f06eaf1ae39c15d212">
             <edge COLOR="#ebd95f"/>
             <font NAME="Helvetica" SIZE="15"/>
-            <node TEXT="libi2padapter" FOLDED="false" POSITION="right" ID="fc860c8b0fe4ba084d38f557">
+            <node TEXT="*.lightworkers domain adapter" FOLDED="false" POSITION="right" ID="1359a0508fb59054fc8ec3be">
                 <edge COLOR="#e096e9"/>
                 <font NAME="Helvetica" SIZE="13"/>
-                <node TEXT="libi2pshepherdsmalltalk" FOLDED="false" POSITION="right" ID="e9ca7c711c3296e5b2adce44">
-                    <edge COLOR="#e095ea"/>
-                    <font NAME="Helvetica" SIZE="12"/>
+                <node TEXT="https://LightAscensionsHumanistsFutureDesignClub.Now.Sh/" FOLDED="false" POSITION="right" ID="03007679054d57223c54bd34">
+                    <edge COLOR="#db8fe6"/>
+                    <font NAME="Helvetica" SIZE="50"/>
+                    <node TEXT="https://gitlab.com/RusCybernetics/LightAscensionsHumanistsFutureDesignClub" FOLDED="false" POSITION="right" ID="a771ee0a186631c9f5c52d8b">
+                        <edge COLOR="#d990e5"/>
+                        <font NAME="Helvetica" SIZE="34"/>
+                    </node>
                 </node>
-                <node TEXT="i2pd [ ]" FOLDED="false" POSITION="right" ID="60c92708b09f86d24de00c51">
-                    <edge COLOR="#e096e9"/>
+            </node>
+            <node TEXT="libi2padapter" FOLDED="false" POSITION="right" ID="fc860c8b0fe4ba084d38f557">
+                <edge COLOR="#7aa3e5"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="libi2pshepherdsmalltalk" FOLDED="false" POSITION="right" ID="e9ca7c711c3296e5b2adce44">
+                    <edge COLOR="#7aa3e5"/>
                     <font NAME="Helvetica" SIZE="12"/>
                 </node>
                 <node TEXT="i2p vanilla [ ]" FOLDED="false" POSITION="right" ID="a226cc71af83ef6f4d476bde">
-                    <edge COLOR="#e096e9"/>
+                    <edge COLOR="#7aa3e5"/>
                     <font NAME="Helvetica" SIZE="12"/>
                 </node>
+                <node TEXT="i2pd [ ]" FOLDED="false" POSITION="right" ID="60c92708b09f86d24de00c51">
+                    <edge COLOR="#7aa3e5"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="Users" FOLDED="false" POSITION="left" ID="5fc63e9296cf0ca139232f95">
+                <edge COLOR="#9ed56b"/>
+                <font NAME="Helvetica" SIZE="13"/>
             </node>
             <node TEXT="OpenCobalt" FOLDED="false" POSITION="right" ID="80a407befa475e4afc30a279">
                 <edge COLOR="#7aa3e5"/>
                 <font NAME="Helvetica" SIZE="13"/>
-                <node TEXT="Squeak!" FOLDED="false" POSITION="right" ID="e1653925f51b01aefc50ad0c">
+                <node TEXT=" ![изображение](attached://a17b8cea3570d13255d48650951eb37c 125x40) 
+http://eeloka.ru/shared/svetliachki/  http://squeak.org/" FOLDED="false" POSITION="right" ID="e1653925f51b01aefc50ad0c">
                     <edge COLOR="#e096e9"/>
-                    <font NAME="Helvetica" SIZE="12"/>
+                    <font NAME="Helvetica" SIZE="8"/>
+                    <node TEXT="See also: Viewpoints Research Institute at http://vpri.org/ and Alan Kay's STEPS project, Tamacola, Nile, OMeta et cetera Et Al" FOLDED="false" POSITION="right" ID="aa4f266f522ac57a381055a4">
+                        <edge COLOR="#e097ea"/>
+                        <font NAME="Helvetica" SIZE="8"/>
+                    </node>
                 </node>
             </node>
             <node TEXT="squ.o.t." FOLDED="false" POSITION="left" ID="9c4fb1ad07c47976609e1a4c">
@@ -110,12 +139,12 @@
                     </node>
                 </node>
             </node>
-            <node TEXT="tranoo ptm java" FOLDED="false" POSITION="right" ID="332eb3ed170b705f989a4922">
+            <node TEXT="tranoo ptm java, он же sl_prj_1" FOLDED="false" POSITION="right" ID="332eb3ed170b705f989a4922">
                 <edge COLOR="#988ee3"/>
                 <font NAME="Helvetica" SIZE="13"/>
                 <node TEXT="Painters" FOLDED="false" POSITION="right" ID="b281438c9d602e751829624c">
                     <edge COLOR="#67d7c4"/>
-                    <font NAME="Helvetica" SIZE="12"/>
+                    <font NAME="Helvetica" SIZE="50"/>
                 </node>
                 <node TEXT="secondlife" FOLDED="false" POSITION="right" ID="436af04aea5c5d81542f718b">
                     <edge COLOR="#968de2"/>
@@ -126,25 +155,25 @@
                     <font NAME="Helvetica" SIZE="12"/>
                 </node>
             </node>
-            <node TEXT="Users" FOLDED="false" POSITION="right" ID="5fc63e9296cf0ca139232f95">
-                <edge COLOR="#9ed56b"/>
-                <font NAME="Helvetica" SIZE="13"/>
-            </node>
             <node TEXT="libmoney" FOLDED="false" POSITION="left" ID="575adaa6de5c314188588480">
                 <edge COLOR="#e9d55f"/>
                 <font NAME="Helvetica" SIZE="13"/>
                 <node TEXT="webwallets::nonlin_fork" FOLDED="false" POSITION="left" ID="bda101d5473935fee5943d48">
                     <edge COLOR="#efa670"/>
                     <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="*.money.lightworkers domain adapter" FOLDED="false" POSITION="left" ID="7d69c66c5b5ce8212a92acc5">
+                        <edge COLOR="#e096e9"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
                     <node TEXT="gostco.in" FOLDED="false" POSITION="left" ID="a3995dd136ba45a39c053778">
                         <edge COLOR="#ead05a"/>
                         <font NAME="Helvetica" SIZE="12"/>
-                        <node TEXT="i2pd" FOLDED="false" POSITION="left" ID="44106081f6f05d2dafccf541">
-                            <edge COLOR="#e9ce63"/>
-                            <font NAME="Helvetica" SIZE="12"/>
-                        </node>
                         <node TEXT="i2p vanilla" FOLDED="false" POSITION="left" ID="f33046843ddf1fec9d341a16">
                             <edge COLOR="#eacd58"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                        <node TEXT="i2pd" FOLDED="false" POSITION="left" ID="44106081f6f05d2dafccf541">
+                            <edge COLOR="#e9ce63"/>
                             <font NAME="Helvetica" SIZE="12"/>
                         </node>
                         <node TEXT="libi2pshepherdnative" FOLDED="false" POSITION="left" ID="a8f72903d308f8620350eaa2">
@@ -194,9 +223,10 @@
 " FOLDED="false" POSITION="right" ID="39d213909847fef2b4b1526f">
                 <edge COLOR="#928be4"/>
                 <font NAME="Helvetica" SIZE="13"/>
-                <node TEXT="thinking about Gaudi and La Sagrada Familia Cathedral" FOLDED="false" POSITION="left" ID="c568abf4a1e3719bdebadbab">
+                <node TEXT="thinking about Gaudi and La Sagrada Familia Cathedral
+ ![изображение](attached://ba072b4bef68d024774631a66b38bae5 150x84) " FOLDED="false" POSITION="left" ID="c568abf4a1e3719bdebadbab">
                     <edge COLOR="#8c86e3"/>
-                    <font NAME="Helvetica" SIZE="25"/>
+                    <font NAME="Helvetica" SIZE="50"/>
                 </node>
             </node>
         </node>
@@ -214,6 +244,101 @@
             <node TEXT="iex.ec" FOLDED="false" POSITION="left" ID="20e18aac6e0db73c5a026439">
                 <edge COLOR="#ee9f69"/>
                 <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="libhardwareproviders" FOLDED="false" POSITION="left" ID="9763f4aed5729eab976a30cb">
+                <edge COLOR="#e096e9"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+        </node>
+        <node TEXT="https://github.com/BIOORGANICOBERON" FOLDED="false" POSITION="right" ID="3faf345ec2b8ae4dd1f91cb5">
+            <edge COLOR="#9ed56b"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="" FOLDED="false" POSITION="right" ID="d6b6c952442f69f8ad230335">
+                <edge COLOR="#7aa3e5"/>
+                <font NAME="Helvetica" SIZE="23"/>
+            </node>
+            <node TEXT="🍄AmanitaBot" FOLDED="false" POSITION="right" ID="c7094195e35042814aa1b6a3">
+                <edge COLOR="#e68782"/>
+                <font NAME="Helvetica" SIZE="23"/>
+                <node TEXT="(currently empty) 
+🍄 https://t.me/AmanitaDwellerBot  
+![изображение](attached://79339d42efde7ba2c7756a92427bb204 150x150) " FOLDED="false" POSITION="right" ID="4640c2ccffa17c16f2f02ebd">
+                    <edge COLOR="#e58885"/>
+                    <font NAME="Helvetica" SIZE="17"/>
+                </node>
+                <node TEXT="https://github.com/RasaHQ
+ ![изображение](attached://514eb7e97e5f6ffbfa2938b9258b8f4d 150x150) " FOLDED="false" POSITION="right" ID="58ef6f227825c71148f0b7bf">
+                    <edge COLOR="#e096e9"/>
+                    <font NAME="Helvetica" SIZE="17"/>
+                </node>
+            </node>
+        </node>
+        <node TEXT="ГОЛУБАЯ ЛУНА, ТО ЕСТЬ МЕЧТА, ИДИОТА, ТОЧНЕЕ ПРОГРАММА-МИНИМУМ В ЛИСТАХ СЕЙ НОДЫ, СПИСОК НОД ПРОГРАММЫ МАКСИМУМ В МИНИМАЛЬНОМ ВАРИАНТЕ ЗАКОНЧЕННОЙ ЦЕЛИ, CODENAME MILESTONE_CRYPTOVR
+
+ ![dreamstimemaximum_55508109](attached://8c5c44db355d154118cb1fccda1453ce 150x120) 
+
+" FOLDED="false" POSITION="left" ID="0cfe5046d8d3405a679fceae">
+            <edge COLOR="#67d7c4"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="Image ID: 55508109" FOLDED="false" POSITION="left" ID="c240b78487c062a972e9a904">
+                <edge COLOR="#5dd5bf"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="Copyright Geforzo | Dreamstime.com" FOLDED="false" POSITION="left" ID="78ee5ff411b5f42500d7ed69">
+                <edge COLOR="#60d8c1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="https://www.dreamstime.com/geforzo_info" FOLDED="false" POSITION="left" ID="b9d53d897eb8b7db713942a5">
+                <edge COLOR="#71d8c5"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="так вот программа-минимум, список нод" FOLDED="false" POSITION="left" ID="9ed400ebfa2cc58a47f9dcc5">
+                <edge COLOR="#62d8c2"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="multiediting this subtree; work in progress" FOLDED="false" POSITION="left" ID="b4cb28f2c8a413c7b8bf8f05">
+                    <edge COLOR="#69d7c1"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="upgrade tranoo ptm to allow for exporting to openmendelevium and to import from blender" FOLDED="false" POSITION="left" ID="b3b33e5f068700a303df08a4">
+                    <edge COLOR="#56d8bd"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="eth" FOLDED="false" POSITION="left" ID="1e2094e07e65daf6790b7bdc">
+                    <edge COLOR="#62d6be"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="webwallets" FOLDED="false" POSITION="left" ID="2591b93ef9ac8d232a6d6910">
+                        <edge COLOR="#67d5bc"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                        <node TEXT="libmoneysmalltalk" FOLDED="false" POSITION="left" ID="f86bed4f6f0067c39210de20">
+                            <edge COLOR="#66d2b8"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                    </node>
+                </node>
+                <node TEXT="openmendelevium app (a fork of opencobalt based on squeak.org smalltalk)" FOLDED="false" POSITION="left" ID="627e0f7810eb71b0f0ce5bc5">
+                    <edge COLOR="#5cd6bd"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="lightlab.tranoo.com" FOLDED="false" POSITION="left" ID="c67937e572f14ee29880a458">
+                    <edge COLOR="#65d9c1"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="CryptoAscensionVR.tranoo.com" FOLDED="false" POSITION="left" ID="3cd822757e4c02cd30e96e25">
+                        <edge COLOR="#63d7bd"/>
+                        <font NAME="Helvetica" SIZE="10"/>
+                        <node TEXT="one .eth addr i hope" FOLDED="false" POSITION="left" ID="3f6e594c6daf7e50aa962233">
+                            <edge COLOR="#9ed56b"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                        <node TEXT="commerce.CryptoAscensionVR.tranoo.com" FOLDED="false" POSITION="left" ID="0fea6e0f96ac5fa1ce4feb2c">
+                            <edge COLOR="#58d6b8"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                            <node TEXT="eth.js metamask and peakd hive and opensea and maybe rarible at commerce.CryptoAscensionVR.tranoo.com" FOLDED="false" POSITION="left" ID="1ec0dc197bdaab565e73619a">
+                                <edge COLOR="#4ed6b4"/>
+                                <font NAME="Helvetica" SIZE="12"/>
+                            </node>
+                        </node>
+                    </node>
+                </node>
             </node>
         </node>
     </node>
